7bacf0bde92c3f39a1a7c3225932d1f24f04b804
yrlee <ttcocoyv12@gmail.com>
2025-02-17 23:00:31 +0900

    feat: init

---
+++ src/test/java/com/stone/soserer/ron/RonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stone.soserer.ron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
